338107b50085c927e40ac202182dbedf79ad09f9
DavideCiammetti <davideciammetti97@gmail.com>
2024-02-04 16:28:09 +0100

    fix: eliminare elementi con js tramite un tasto di conferma

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,3 +7,22 @@ import * as bootstrap from "bootstrap";
 
 //process images
 import.meta.glob(["../img/**"]);
+
+for(let i = 0; i < 20; i++){
+const button = document.querySelector('.button-annulla-'+i);
+let formContainer = document.querySelector('.form-container-'+i);
+const deleteButton = document.querySelector('.js-delete-'+i);
+
+
+    // mostra alert
+deleteButton.addEventListener('click', function(){
+    console.log(1);
+    formContainer.classList.remove("d-none");
+    formContainer.style.display = "block";
+});
+// nascondi alert
+button.addEventListener('click', function(){
+    console.log(2);
+    formContainer.classList.add("d-none");
+});
+}
--- resources/views/comicsManagement/showList.blade.php
+++ resources/views/comicsManagement/showList.blade.php
@@ -4,8 +4,9 @@
     <main id="main">
         {{-- inserimento cards che mostrano descrizione, foto, titolo, data, prezzo --}}
         <div class=" d-flex flex-wrap justify-content-center gap-3 mt-5 mb-5"> 
-            @foreach ($comics as $comic)
-            <div class="box-shad-g card" style="width: 12rem;">
+            @foreach ($comics as $key=> $comic)
+
+            <div class="box-shad-g card position-relative" style="width: 12rem;" >
                 <img src="{{ $comic->thumb }}" class="card-img-top" alt="img">
                 <div class="card-body">
                     <h5 class="card-title">{{$comic->title}}</h5>
@@ -16,17 +17,25 @@
                     <p>price: {{$comic->price}} $</p>
                     <p>date: {{$comic->sale_date}}</p>
                    <div class="d-flex flex-column">
-                        <span class=" "><a href="{{route('comics.show', $comic->id)}}">vedi dettagli</a></span>
+                        <span><a href="{{route('comics.show', $comic->id)}}">vedi dettagli</a></span>
                         <span class="mb-1"><a href="{{route('comics.edit', $comic->id)}}">modifica</a></span>
                    </div>
                    <div>
-                        <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-
-                            <input type="submit" value="DELETE">
-                        </form>
+                    <input class="js-delete-{{$key}}" type="submit" value="delete">
                    </div>
+                   {{-- form container --}}
+                   <div class="delete-form alert alert-danger position-absolute form-container-{{$key}} d-none" role="alert">
+                       <p class="f-size-alert"> se vuoi eliminarlo premi Delete altrimenti annulla</p>
+                        <div class="d-flex">
+                            <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                                @csrf
+                                @method('DELETE')
+    
+                                <input type="submit" value="Delete">
+                            </form>
+                            <input class="ms-2 button-annulla-{{$key}}" type="submit" value="Annulla">
+                        </div>
+                  </div>
                 </div>
             </div>
             @endforeach
